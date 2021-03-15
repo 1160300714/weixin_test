dad84d8c9527a0795e2689dd0cfce15f7090fe31
Link <694515124@qq.com>
2021-03-15 08:57:55 +0800

    update

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,31 +1,14 @@
 Page({
   data: {
-    list: []
+    items: ['事项 A', '事项 B', '事项 C']
   },
-  onShow() {
-    console.log(app.globalData.userInfo)
-    var that=this;
+  onLoad() {
+    const that = this;
     wx.request({
-      url: 'http://localhost/showlist.php',
-      data: {},
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      method: 'POST',
-      success: function(res) {
-        console.log(res.data)
-        if (res.data.constructor === Array) {
-          that.setData({
-            list: res.data
-          })
-        }else{
-          wx.showModal({
-            title: '暂无数据',
-            content: '请发布小目标',
-          })
-        }
+      url: 'http://localhost/uptxt.php',
+      success(res) {
+        that.setData({ items: res.data });
       }
-
-    })
+    });
   }
-})
+});
